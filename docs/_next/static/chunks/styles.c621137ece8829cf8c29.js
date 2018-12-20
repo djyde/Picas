c2a4bc2ac21e71ddc06f8d705e4261ec8ccdd4c8
Randy <randypriv@gmail.com>
2018-12-20 17:44:51 +0800

    remove docs

--- docs/_next/static/chunks/styles.c621137ece8829cf8c29.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{219:function(n,w,o){}}]);
--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-const withSCSS = require('@zeit/next-sass')
-const isProd = process.env.NODE_ENV === 'production'
+const { PHASE_PRODUCTION_SERVER } = process.env.NODE_ENV === 'development' ? {} : require('next-server/constants');
 
-module.exports = withSCSS({
-  assetPrefix: isProd ? 'https://djyde.github.io/Picas' : ''
-})
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_PRODUCTION_SERVER) {
+    
+    return {
+      /* production only config */
+    }
+  }
+  const withCSS = require('@zeit/next-sass')
+  return withCSS({})
+}
