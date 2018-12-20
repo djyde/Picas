3f716a2ae556ff4d08f09b8a46738f267382f933
Randy <randypriv@gmail.com>
2018-12-20 17:25:39 +0800

    change prefix

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 const withSCSS = require('@zeit/next-sass')
+const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = withSCSS()
+module.exports = withSCSS({
+  assetPrefix: isProd ? 'https://djyde.github.io/Picas' : ''
+})
